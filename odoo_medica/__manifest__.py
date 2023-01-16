d58d5b7f3eee23822e44070462239728d284a441
ivan <odootest725@gmail.com>
2023-01-16 18:55:15 +0000

    [ADD] ModuloMedica

---
+++ odoo_medica/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Medica',
+    
+    'summary':"""Medica app to manage Training""",
+    
+    'description': """
+        Academy Module to manage :
+        -Citas
+        -Medicos
+        -Pacientes
+    """,
+    
+    'author': 'Ivan',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo':[
+       
+    ],
+    
+     'license': 'LGPL-3',
+    
+}
